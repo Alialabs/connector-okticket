a2b95ad79ef8cced1624457a32bf57b1c4149aca
cesar <cesarportas@alialabs.com>
2024-07-02 16:55:11 +0200

    FIX: Change prints to logger

--- okticket_connector_hr_expense_sheet/models/okticket_hr_expense_sheet_exporter.py
+++ okticket_connector_hr_expense_sheet/models/okticket_hr_expense_sheet_exporter.py
@@ -60,7 +60,7 @@ class HrExpenseExporter(Component):
 
         expense_sheet = self.env['hr.expense.sheet'].browse(expense_sheet.id)
         expense_sheet.write({'name': new_name_to_test})
-        print(f'Creating conficting in okticket {expense_sheet.name}')
+        _logger.info('Creating conficting in okticket {expense_sheet.name}')
         res = backend_adapter.create(expense_sheet)
         if not res:
             raise Exception(_("(generate_new_expense_sheet): It is not possible to find a valid name for "
@@ -96,7 +96,7 @@ class HrExpenseExporter(Component):
             binding = expense_sheet.okticket_bind_ids and \
                       expense_sheet.okticket_bind_ids[0] or False
             if not binding:
-                print(f'Creating in okticket {expense_sheet.name}')
+                _logger.info(f'Creating in okticket {expense_sheet.name}')
                 creation_result = backend_adapter.create(expense_sheet)
                 if not creation_result:
                     # New expenses sheet
